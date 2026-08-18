293a6f0559efbb8be4b36ad80eb1a0a05425feb8
shruuu158 <shrutidevkate2004@gmail.com>
2026-08-18 22:58:39 +0530

    Time: 486 ms (16.14%), Space: 47.8 MB (99.63%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.java
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.java
@@ -1,38 +1,31 @@
 class Solution {
     public int maxProduct(String[] words) {
-
-        int max = 0;
-
-        for (int i = 0; i < words.length; i++) {
-
-            boolean[] seen = new boolean[26];
-
-            // Store characters of first word
-            for (char ch : words[i].toCharArray()) {
-                seen[ch - 'a'] = true;
+        int max=0;
+        for(int i=0;i<words.length;i++){
+            boolean b[]=new boolean[26];
+            for(int j=0;j<words[i].length();j++){
+                char ch=words[i].charAt(j);
+                b[ch-'a']=true;
             }
-
-            for (int j = i + 1; j < words.length; j++) {
-
-                boolean common = false;
-
-                // Check second word
-                for (char ch : words[j].toCharArray()) {
-
-                    if (seen[ch - 'a']) {
-                        common = true;
-                        break;
+              
+            for(int j=i+1;j<words.length;j++){
+                          boolean com=false;
+                    for(int k=0;k<words[j].length();k++){
+                        char ch=words[j].charAt(k);
+                        if(b[ch-'a']){
+                            com=true;
+                            break;
+                        }
                     }
-                }
-
-                // No common character
-                if (!common) {
-                    int product = words[i].length() * words[j].length();
-                    max = Math.max(max, product);
-                }
+                    if(!com){
+                        int prodcut=words[i].length()*words[j].length();
+                        max=Math.max(max,prodcut);
+                    }
             }
+            
         }
-
-        return max;
+           
+            return max;
+    
     }
 }
