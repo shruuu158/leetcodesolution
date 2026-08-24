e281aeaa8912f9764fcf31c6289e9fc8bb0284a1
shruuu158 <shrutidevkate2004@gmail.com>
2026-08-24 22:13:29 +0530

    Time: 34 ms (72.17%), Space: 48.3 MB (46.46%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,17 +1,17 @@
 class Solution {
+
     public String removeStars(String s) {
-
-        StringBuilder sb = new StringBuilder();
-
-        for (char ch : s.toCharArray()) {
-
-            if (ch == '*') {
-                sb.deleteCharAt(sb.length() - 1);
-            } else {
-                sb.append(ch);
-            }
+        StringBuilder sb=new StringBuilder();
+        int i=0;
+       while(i<s.length()){
+        if(s.charAt(i)=='*'){
+            sb.deleteCharAt(sb.length()-1);
+            i++;
+        }else{
+            sb.append(s.charAt(i));
+            i++;
         }
-
-        return sb.toString();
+       }
+       return sb.toString();
     }
 }
